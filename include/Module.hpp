82cc1bf843ab0f8f11766cc84d50c97dd4e0aea2
Asphähyre <luka.boulagnon@epitech.eu>
2017-05-14 00:49:36 +0200

    Updated Modules inside GameCore

--- include/Module.hpp
+++ include/Module.hpp
@@ -4,25 +4,55 @@
 #include <vector>
 #include "BasicExcel.hpp"
 
-class Text {
- public:
-  Text(YExcel::BasicExcelWorksheet const*);
+struct Consequence {
+  std::map<std::string, int> variables;
+};
+
+struct Results {
+  std::string direction;
+  Consequence consequences;
+};
+
+template <class EType>
+struct EventAction {
+  std::vector<EType> reactions;
 };
 
 class IModule {
-  IModule* getNext() const;
-
- private:
-  std::vector<std::string> childs;
+ public:
+  virtual Results const& getConsequences() const = 0;
 };
 
 template <class Type>
 class Module : public IModule {
  public:
-  Module(Type contained) : content(contained) {}
+  Module(Type contained) : content(contained) {
+    results.direction = "Next";
+    results.consequences.variables[std::string("toto")] = 18;
+  }
+  EventAction<typename Type::EventType> const& getEvent();
+  void setReact(size_t);
+  Results const& getConsequences() const { return results; }
 
  private:
+  Results results;
   Type content;
+  Consequence cnsq;
+  std::vector<std::string> childs;
+};
+
+/*
+** ===============================
+** Modules types
+** ===============================
+*/
+
+class Text {
+ public:
+  struct EventType {
+    std::string event;
+  };
+  Text(YExcel::BasicExcelWorksheet const*);
 };
 
 #endif /* !MODULE_HPP */

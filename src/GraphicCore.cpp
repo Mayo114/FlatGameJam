c8b85415a747d76e73574a3aaf573572c1b3f216
rougag_b <boris.rougagnou@epitech.eu>
2017-05-14 01:34:02 +0200

    GraphicCore correct

--- src/GraphicCore.cpp
+++ src/GraphicCore.cpp
@@ -8,12 +8,21 @@ GraphicCore::GraphicCore() {}
 GraphicCore::~GraphicCore() {}
 
 void GraphicCore::start() {
-  this->win = new sf::RenderWindow(sf::VideoMode(1024, 768, 32), "Skiving",
-				   sf::Style::Fullscreen);
+  this->win = new sf::RenderWindow(sf::VideoMode(1920, 1080, 32), "Skiving",
+				   sf::Style::Fullscreen | sf::Style::Close);
 }
 
-int GraphicCore::menu() {
-  sf::RenderTexture background;
+// void Button::eventMouse(sf::Event * event, sf::RenderWindow * window)
+// {
+//   sf::FloatRect	mousePosition(event->mouseButton.x,
+// 			      event->mouseButton.y, 1, 1);
+
+//   if(m_spriteButton.getGlobalBounds().intersects(mousePosition))
+//     std::cout << "Yes!" << std::endl;
+// }
+
+int GraphicCore::menu()
+{
   sf::Texture t_background;
   sf::Sprite s_background;
   sf::Texture t_title;
@@ -26,10 +35,11 @@ int GraphicCore::menu() {
   if (!t_background.loadFromFile("./assets/menu/background.jpg") ||
       !t_title.loadFromFile("./assets/menu/skiving_title.png") ||
       !t_play.loadFromFile("./assets/menu/play.png") ||
-      !t_exit.loadFromFile("./assets/menu/quit.png")) {
-    std::cout << "impossible open asset file" << std::endl;
-    return (0);
-  }
+      !t_exit.loadFromFile("./assets/menu/quit.png"))
+    {
+      std::cout << "impossible open asset file" << std::endl;
+      return (0);
+    }
   t_title.setSmooth(false);
   s_title.setTexture(t_title);
   t_background.setSmooth(true);
@@ -38,24 +48,37 @@ int GraphicCore::menu() {
   s_play.setTexture(t_play);
   t_exit.setSmooth(false);
   s_exit.setTexture(t_exit);
-  background.clear(sf::Color(0, 0, 0));
-  background.draw(s_background);
-  background.draw(s_title);
-  background.draw(s_play);
-  background.draw(s_exit);
-  while (this->win->isOpen()) {
-    sf::Event event;
-    while (this->win->pollEvent(event)) {
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) ||
-	  event.type == sf::Event::Closed) {
-	this->win->close();
-	return -1;
-      }
+
+  // centrer une image sur un écran: ((res écran - res image) / 2)
+  // ajouter image au bout de l'écran - 10: (res écran - (res image + 10))
+  //  s_title.setTextureRect(sf::IntRect(250, 500, 500, 100));
+  s_title.setPosition(sf::Vector2f(((1920 - 848) / 2), 10));
+
+  //  s_play.setTextureRect(sf::IntRect(250, 200, 500, 100));
+  s_play.setPosition(sf::Vector2f(10, 200));
+
+  //  s_exit.setTextureRect(sf::IntRect(250, 350, 500, 100));
+  s_exit.setPosition(sf::Vector2f((1920 - ((848 /2) + 10)), 350));
+
+  while (this->win->isOpen())
+    {
+      sf::Event event;
+      while (this->win->pollEvent(event))
+	{
+	  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) ||
+	      event.type == sf::Event::Closed)
+	    {
+	      this->win->close();
+	      return -1;
+	    }
+	}
+      this->win->clear(sf::Color(0, 0, 0));
+      this->win->draw(s_background);
+      this->win->draw(s_title);
+      this->win->draw(s_play);
+      this->win->draw(s_exit);
+      this->win->display();
     }
-    this->win->clear();
-    this->win->draw(sf::Sprite(background.getTexture()));
-    this->win->display();
-  }
   return -1;
 }
 
@@ -63,26 +86,13 @@ void GraphicCore::loop() {
   while (this->win->isOpen()) {
     sf::Event event;
     while (this->win->pollEvent(event)) {
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) ||
-	  event.type == sf::Event::Closed) {
-	this->win->close();
-	return;
+      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+	std::cout << "In the if :)" << std::endl;
+	exit(0);
       }
+      if (event.type == sf::Event::Closed) this->win->close();
     }
     this->win->clear();
     this->win->display();
   }
-}
-
-//class Event
-//Reaction
-//Consequence
-
-void		GraphicCore::dispModule(Module<Text> const & m)
-{
-  // getEvent --> dispEvent if not null
-
-  // si action --> m.setReact(size_t);
-  //
-  // m.getCnsq() const;
 }

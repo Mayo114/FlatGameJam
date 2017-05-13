8b289df3742f2150766c9accba37aada23916258
Asphähyre <luka.boulagnon@epitech.eu>
2017-05-13 14:58:37 +0200

    Added SFML test app

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,24 @@
 // Last update sam. mai 13 14:41:16 2017 Luka Boulagnon
 //
 
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello world!" << std::endl;
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) window.close();
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
   return 0;
 }
